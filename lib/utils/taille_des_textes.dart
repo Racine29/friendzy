885566926f3972f0bbe57161164e541819ed1b2a
Racine Sy <racinesy29@gmail.com>
2023-11-22 17:27:18 +0000

    ajout de la class taille de texte

---
+++ lib/utils/taille_des_textes.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+import 'package:friendzy/utils/poids_du_texte.dart';
+import 'package:friendzy/utils/taille_des_polices.dart';
+
+class TailleDuText{
+
+static TextStyle texte12Normal(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s12px,
+  fontWeight: PoidsDeLaPolice.normal,
+);
+ 
+
+static TextStyle texte16Normal(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s16px,
+  fontWeight: PoidsDeLaPolice.normal,
+);
+static TextStyle texte16DemiGras(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s16px,
+  fontWeight: PoidsDeLaPolice.demiGras,
+);
+static TextStyle texte16Gras(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s16px,
+  fontWeight: PoidsDeLaPolice.gras,
+);
+ 
+ 
+static TextStyle texte18DemiGras(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s18px,
+  fontWeight: PoidsDeLaPolice.demiGras,
+);
+static TextStyle texte18Gras(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s18px,
+  fontWeight: PoidsDeLaPolice.gras,
+);
+ 
+ 
+ 
+static TextStyle texte24DemiGras(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s24px,
+  fontWeight: PoidsDeLaPolice.demiGras,
+);
+ 
+static TextStyle texte32Gras(Color color  ) => TextStyle(
+  color: color,
+  fontSize: s32px,
+  fontWeight: PoidsDeLaPolice.gras,
+);
+ 
+
+}
