a15388b9746f7e8fde627930f2b975afcce60a41
Racine Sy <racinesy29@gmail.com>
2023-11-22 17:25:53 +0000

    ajout des couleurs

--- lib/utils/couleurs.dart
+++ lib/utils/couleurs.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 const Color couleurPrincipal = Color(0xff4B164C);
 const Color couleurSecondaire = Color(0xffFEEFF7);
 const Color couleurArrierePlan = Color(0xffFDF7FD);
+const Color texteCouleurNoir = Color(0xff22172A);
+const Color texteCouleurVoilet = Color(0xff4B164C);
+const Color texteCouleurBlanc = Color(0xffffffff);
