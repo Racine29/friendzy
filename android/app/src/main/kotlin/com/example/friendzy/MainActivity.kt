36e87ef0b574953054fab9324c114653cf8be839
Racine Sy <racinesy29@gmail.com>
2023-11-22 13:34:04 +0000

    initialisation du projet

---
+++ android/app/src/main/kotlin/com/example/friendzy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.friendzy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/couleurs.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color couleurPrincipal = Color(0xff4B164C);
+const Color couleurSecondaire = Color(0xffFEEFF7);
+const Color couleurArrierePlan = Color(0xffFDF7FD);
