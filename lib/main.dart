512ec424986476bba7422cb1e3827beeed678b1f
Racine Sy <racinesy29@gmail.com>
2023-11-22 17:09:16 +0000

    ajout du plugin pour adapter l'appli a tous type d'ecran

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 void main(){
   runApp(MyApp());
@@ -9,13 +10,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "Hellix"),
-      // initialRoute: ,
-      routes: {
-        
-      },
+    return Sizer(
+      builder: (context,_,__) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(fontFamily: "Hellix"),
+          // initialRoute: ,
+          routes: {
+            
+          },
+        );
+      }
     );
   }
 }
