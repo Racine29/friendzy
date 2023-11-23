8c045eedf0e31c94910b4827b696cefd797d6ea2
Racine Sy <racinesy29@gmail.com>
2023-11-23 11:41:37 +0000

    mise a jours du dossier utilitaire

---
+++ lib/utilitaires/couleurs.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color couleurPrincipal = Color(0xff4B164C);
+const Color couleurSecondaire = Color(0xffDF8FD2);
+const Color couleurTertiaire = Color(0xffFEEFF7);
+const Color couleurArrierePlan = Color(0xffFDF7FD);
+const Color texteCouleurNoir = Color(0xff22172A);
+const Color texteCouleurVoilet = Color(0xff4B164C);
+const Color texteCouleurBlanc = Color(0xffffffff);
