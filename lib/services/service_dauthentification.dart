1e8522575be3ec93000647838ef084b6d71de100
Racine Sy <racinesy29@gmail.com>
2023-12-02 11:19:02 +0000

    ajout de firebase auth et de la collection utilisateur

--- lib/services/service_dauthentification.dart
+++ lib/services/service_dauthentification.dart
@@ -1,5 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:friendzy/modeles/utilisateur_model.dart';
 
 class ServicesDauthentifications {
   final authentification = FirebaseAuth.instance;
+  final utilisateurCollection =
+      FirebaseFirestore.instance.collection("utilisateur");
+  User? get utilisateurActuel => authentification.currentUser;
+
+  Stream<User?> get estConnecter => authentification.authStateChanges();
+
+  Stream<UtilisateurModel> get donneesDeLutilisateur {
+    return utilisateurCollection.doc(utilisateurActuel?.uid).snapshots().map(
+        (utilisateur) => UtilisateurModel.deJSON(
+            utilisateur.data() as Map<String, dynamic>));
+  }
 }
