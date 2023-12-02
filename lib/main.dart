ae733b5d42699ca89255833155979291e80d7149
Racine Sy <racinesy29@gmail.com>
2023-12-02 11:15:36 +0000

    ajout de nouveaux ecrans sur la route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:friendzy/firebase_options.dart';
 import 'package:friendzy/fournisseurs/utilisateur_fournisseur.dart';
+import 'package:friendzy/pages/ecran_emballage.dart';
 import 'package:friendzy/pages/inscription/etape/centre_dinteret.dart';
 import 'package:friendzy/pages/inscription/etape/nom.dart';
 import 'package:friendzy/pages/inscription/etape/photo.dart';
@@ -8,10 +11,13 @@ import 'package:friendzy/pages/inscription/via-numero-telephone/entrer_code_de_c
 import 'package:friendzy/pages/inscription/via-numero-telephone/entrer_numero_de_telephone.dart';
 import 'package:friendzy/pages/onboarding/onboarding.dart';
 import 'package:friendzy/pages/onboarding/onboarding_reseau_social.dart';
+import 'package:friendzy/pages/seFaireDesAmis/accueil.dart';
 import 'package:sizer/sizer.dart';
 import "package:provider/provider.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -32,6 +38,8 @@ class MyApp extends StatelessWidget {
             initialRoute: Onboarding.page,
             routes: {
               Onboarding.page: (context) => Onboarding(),
+              EcranEmballage.page: (context) => EcranEmballage(),
+              Accueil.page: (context) => Accueil(),
               OnboardingReseauSocial.page: (context) =>
                   OnboardingReseauSocial(),
               EntrerNumeroDeTelephone.page: (context) =>
