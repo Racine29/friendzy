ca1f4866aabde32443ea7ad3f737d90ac16aa8c6
Racine Sy <racinesy29@gmail.com>
2023-12-20 17:50:26 +0000

    mise a jour des conditions d'appiration d'ecran en fonction des donnees de mon utilisateur

--- lib/pages/ecran_emballage.dart
+++ lib/pages/ecran_emballage.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:friendzy/modeles/utilisateur_model.dart';
+import 'package:friendzy/modeles/utilisateur_modele.dart';
+import 'package:friendzy/pages/inscription/etape/etape_accueil.dart';
 import 'package:friendzy/pages/onboarding/onboarding_reseau_social.dart';
+import 'package:friendzy/pages/utilisateur_connecter/bar_de_navigation.dart';
 import 'package:friendzy/services/service_dauthentification.dart';
 import 'package:provider/provider.dart';
-import 'package:friendzy/pages/seFaireDesAmis/accueil.dart';
 
 class EcranEmballage extends StatefulWidget {
   const EcranEmballage({super.key});
@@ -14,18 +15,23 @@ class EcranEmballage extends StatefulWidget {
 }
 
 class _EcranEmballageState extends State<EcranEmballage> {
+  final service = ServicesDauthentifications();
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: ServicesDauthentifications().estConnecter,
+      stream: service.estConnecter,
       builder: (context, AsyncSnapshot<User?> utilisateur) {
-        if (utilisateur.hasData) {
+        print("Le nom de mon est -${utilisateur.data?.displayName}");
+        if (utilisateur.hasData && utilisateur.data?.displayName != null) {
           return StreamProvider<UtilisateurModel>(
-            create: (context) =>
-                ServicesDauthentifications().donneesDeLutilisateur,
+            create: (_) => service.donneesDeLutilisateur,
             initialData: UtilisateurModel(),
-            child: Accueil(),
+            child: BarDeNavigation(),
           );
+        } else if (utilisateur.hasData &&
+            utilisateur.data?.displayName == null) {
+          return const EtapeAccueil();
         }
         return const OnboardingReseauSocial();
       },
